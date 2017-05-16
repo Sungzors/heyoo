5475df100308b0a3857dd1d1a628ea3cb011521a
Sung Won Chun <s1chun3636@gmail.com>
2017-05-16 14:38:00 -0700

    sets layout to adapter

--- app/src/main/java/com/phdlabs/sungwon/heyoo/utility/adapter/EventRecyclerAdapter.java
+++ app/src/main/java/com/phdlabs/sungwon/heyoo/utility/adapter/EventRecyclerAdapter.java
@@ -2,9 +2,14 @@ package com.phdlabs.sungwon.heyoo.utility.adapter;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.phdlabs.sungwon.heyoo.R;
 import com.phdlabs.sungwon.heyoo.model.HeyooEvent;
+import com.phdlabs.sungwon.heyoo.utility.BaseViewHolder;
+import com.phdlabs.sungwon.heyoo.utility.ViewMap;
 
 /**
  * Created by SungWon on 5/15/2017.
@@ -37,10 +42,35 @@ public class EventRecyclerAdapter extends BaseListRecyclerAdapter<HeyooEvent, Re
     }
 
     @Override
-    protected RecyclerView.ViewHolder viewHolder(LayoutInflater inflater, ViewGroup parent, int type) {
+    protected RecyclerView.ViewHolder viewHolder(LayoutInflater inflater, final ViewGroup parent, int type) {
         switch (type){
             case 0:/*Title*/
-                break;
+                return new BaseViewHolder(R.layout.card_view_event_title, inflater, parent){
+                    @Override
+                    protected void addClicks(ViewMap views) {
+                        views.click(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                switch(view.getId()){
+                                    case R.id.cvet_yes_button:
+                                        Toast.makeText(parent.getContext(), "Yes button clicked", Toast.LENGTH_SHORT).show();
+                                        break;
+                                    case R.id.cvet_maybe_button:
+                                        Toast.makeText(parent.getContext(), "Maybe button clicked", Toast.LENGTH_SHORT).show();
+                                        break;
+                                    case R.id.cvet_no_button:
+                                        Toast.makeText(parent.getContext(), "No button clicked", Toast.LENGTH_SHORT).show();
+                                        break;
+                                }
+                            }
+                        });
+                    }
+
+                    @Override
+                    protected void putViewsIntoMap(ViewMap views) {
+                        views.put(R.id.cvet_event_title, R.id.cvet_event_date, R.id.cvet_yes_button, R.id.cvet_maybe_button, R.id.cvet_no_button);
+                    }
+                };
             case 1:/*Image*/
                 break;
             case 2:/*Statistics?*/
@@ -51,5 +81,9 @@ public class EventRecyclerAdapter extends BaseListRecyclerAdapter<HeyooEvent, Re
                 break;
         }
         return null;
+    }
+
+    private void bindTitleHolder(BaseViewHolder baseViewHolder, HeyooEvent event){
+
     }
 }
