be6146ca946bd1b73f2b80a1a3b39a9f3ee2bd62
Sung Won Chun <s1chun3636@gmail.com>
2017-07-21 17:25:55 -0700

    gradle dependencies massive clean up, add gcm

---
+++ app/src/main/java/com/phdlabs/sungwon/heyoo/api/utility/gms/RegistrationService.java
@@ -0,0 +1,21 @@
+package com.phdlabs.sungwon.heyoo.api.utility.gms;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by SungWon on 7/21/2017.
+ */
+
+public class RegistrationService extends IntentService {
+
+    public RegistrationService() {
+        super("RegistrationService");
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+
+    }
+}
