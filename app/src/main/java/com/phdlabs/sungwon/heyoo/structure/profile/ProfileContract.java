90959dfdfec76a15bd81594773ecd2b5ad9fbff5
Sung Won Chun <s1chun3636@gmail.com>
2017-07-27 12:20:44 -0700

    profile package and empty activity to fragment init

---
+++ app/src/main/java/com/phdlabs/sungwon/heyoo/structure/profile/ProfileContract.java
@@ -0,0 +1,9 @@
+package com.phdlabs.sungwon.heyoo.structure.profile;
+
+/**
+ * Created by SungWon on 7/27/2017.
+ */
+
+public interface ProfileContract {
+
+}
---
+++ app/src/main/java/com/phdlabs/sungwon/heyoo/structure/profile/ProfileFragment.java
@@ -0,0 +1,8 @@
+package com.phdlabs.sungwon.heyoo.structure.profile;
+
+/**
+ * Created by SungWon on 7/27/2017.
+ */
+
+public class ProfileFragment {
+}
