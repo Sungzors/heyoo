87c1fffeeb6038d78e208a892b5874059b356ef9
Sung Won Chun <s1chun3636@gmail.com>
2017-08-01 12:35:36 -0700

    importing twilio library

--- app/src/main/java/com/phdlabs/sungwon/heyoo/structure/admessages/MessageFragment.java
+++ app/src/main/java/com/phdlabs/sungwon/heyoo/structure/admessages/MessageFragment.java
@@ -1,6 +1,10 @@
 package com.phdlabs.sungwon.heyoo.structure.admessages;
 
+import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 import com.phdlabs.sungwon.heyoo.structure.core.BaseFragment;
 
@@ -11,6 +15,16 @@ import com.phdlabs.sungwon.heyoo.structure.core.BaseFragment;
 public class MessageFragment extends BaseFragment<MessageContract.Controller>
         implements MessageContract.View{
 
+    private RecyclerView mRecycler;
+
+
+    public static MessageFragment newInstance(){
+        Bundle args = new Bundle();
+        MessageFragment fragment = new MessageFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
     @NonNull
     @Override
     protected MessageContract.Controller createController() {
@@ -20,5 +34,16 @@ public class MessageFragment extends BaseFragment<MessageContract.Controller>
     @Override
     protected int layoutId() {
         return 0;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
     }
 }
